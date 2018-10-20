85491985464fcba532f748fa44c6f6e00d18e0a6
Hannu Hytönen <hannu.hytonen@eng.tamk.fi>
2018-10-20 18:39:21 +0300

    testi

---
+++ Osa 2_Hannu Hytönen.py
@@ -0,0 +1,5 @@
+print("#1 Mitä hyötyjä automaattisesta testauksesta on verrattuna manuaaliseen testaukseen?")
+print("#2 Mitä haasteita testauksen automatisointi asettaa verrattuna manuaaliseen testaukseen? Pohdi näitä erityisesti kurssin sisällön kontekstissa.")
+print("#3 Mihin tarvitaan versionhallintaa? Pohdi asiaa testausprojektin kannalta, jossa on useita rinnakkaisia testaajia tai tiimejä. Minkälaisia haasteita saatat kohdata?")
+print("#4 Mitä etuja Python tarjoaa kielenä sulautetun järjestelmän testaukseen verrattuna esim. C-kieleen? Mitä haasteita?")
+print("#5 Miten testiautomaatiota voisi soveltaa kurssilla tehtyihin suunnitteluesimerkkeihin?")
